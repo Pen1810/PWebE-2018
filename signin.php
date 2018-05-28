c7a668946ca229e166133c6fb272baaff5d9eeb3
ojoakua-10bit <mirror.ojoakua@gmail.com>
2018-05-28 16:59:03 +0700

    signin harusnya bisa

--- signin.php
+++ signin.php
@@ -1,7 +1,58 @@
 <?php
 require_once ("config.php");
+$username = $password = "";
+$error = false;
 
+if(isset($_POST['submit'])){
+    if(empty(trim($_POST["username"]))){
+        $error = true;
+    }
+    else{
+        $username = trim($_POST["username"]);
+    }
 
+    if(empty(trim($_POST['password']))){
+        $error = true;
+    }
+    else{
+        $password = trim($_POST['password']);
+    }
+
+    if(empty($username_err) && empty($password_err)){
+        $sql = "SELECT username, pass FROM member WHERE username = ?";
+
+        if($stmt = mysqli_prepare($usrconn, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            $param_username = $username;
+
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            session_start();
+                            $_SESSION['username'] = $username;
+                            header("location: index.php");
+                        }
+                        else{
+                            $error = true;
+                        }
+                    }
+                }
+                else{
+                    $error = true;
+                }
+            }
+            else{
+                die("Oops! Something went wrong. Please try again later.");
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($usrconn);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,6 +70,13 @@ require_once ("config.php");
             <h2>Sign-in to Sample Shopping</h2>
         </div>
     </div>
+    <?php
+    if (isset($_GET['register']) && $_GET['register'] == "success"){
+        echo "<div class='row'>";
+        echo "<div class='col-sm-12 alert alert-success'>Anda telah terdaftar. Silakan masuk menggunakan akun Anda.</div>";
+        echo "</div>";
+    }
+    ?>
     <div class="row">
         <div class="col-sm-12">
             <form name="signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
@@ -35,7 +93,7 @@ require_once ("config.php");
                     <?php if ($error) echo "<div class='alert alert-danger'>Username atau password tidak valid!</div>" ?>
                 </div>
                 <div>
-                    <input type="submit" value="Sign Up" name="submit" class="btn btn-primary" id="submitform">
+                    <input type="submit" value="Sign In" name="submit" class="btn btn-primary" id="submitform">
                 </div>
             </form>
         </div>
