1ecb2c1ea99d5e63ff00f20515e1e5652cd25ff2
Akmal Darari <akmalflightsimmer@gmail.com>
2018-05-28 18:13:18 +0700

    abcde

--- template.php
+++ template.php
@@ -41,49 +41,42 @@ require_once("config.php");
 
 <body>
 <ul>
-    <li><img src="img/JB.png" alt=""/></li>
     <li><a href="index.php" class="smoothScroll">Home</a></li>
     <li><a href="shop.php" class="smoothScroll">Shop</a></li>
-    <li><a href="#contact" class="smoothScroll">Contact</a></li>
-    <li><img onclick="changeEng()" src="img/eng.png" style="cursor: pointer; margin:12px 0 0 800px; height:20px;">
-    </li>
-    <li><img onclick="changeInd()" src="img/ina.jpg"
-             style="cursor: pointer; margin:7px 0 0 850px; margin-top:-40px; height:30px;"></li>
+    <li><a href="#" class="smoothScroll">About</a></li>
+    <li style="float:right"><a href="signup.php" class="smoothScroll">Sign Up</a></li>
+    <li style="float:right"><a href="signin.php" class="smoothScroll">Sign in</a></li>
+
 </ul>
 
+<!-- Main source -->
+<div id="container" class="bounceInDown animated">
+    
+</div>
+
 <!-- ========== FOOTER SECTION ========== -->
-<section style="margin-top:75px" id="contact" name="contact"></section>
-<div id="f">
+<section id="contact" name="contact"></section>
+<div id="f" style="background-color:#333">
     <div class="container">
         <div class="row">
-            <h3><b>CONTACT US</b></h3>
-            <br>
-            <div class="col-lg-4">
+            <div class="col-sm-12">
+                <h3 class="text-center"><b>CONTACT US</b></h3>
+            </div>
+            <div class="col-sm-6">
                 <h3><b>Send Us A Message:</b></h3>
-                <h3 class="text-foot">justbatik@gmail.com</h3>
+                <h3 class="text-foot">turanwash@gmail.com</h3>
                 <br>
             </div>
 
-            <div class="col-lg-4">
+            <div class="col-sm-6">
                 <h3><b>Call Us:</b></h3>
-                <h3 class="text-foot">+62 8123 2996 876</h3>
-                <br>
-            </div>
-
-            <div class="col-lg-4">
-                <h3><b>We Are Social</b></h3>
-                <p>
-                    <a href="index.php#"><i class="icon-facebook"></i></a>
-                    <a href="index.php#"><i class="icon-twitter"></i></a>
-                    <a href="index.php#"><i class="icon-envelope"></i></a>
-                </p>
+                <h3 class="text-foot">+6281234567890</h3>
                 <br>
             </div>
         </div>
     </div>
 </div><!-- /container -->
 </div><!-- /f -->
-<!-- dialog box -->
 
 
 <!-- script of dialog -->
@@ -92,13 +85,12 @@ require_once("config.php");
     let whitebg = document.getElementById("white-background");
     let sizebox = document.getElementById("size-box");
     let dsgn = document.getElementById("design-con");
-    let buybox = document.getElementById("buy-box");
     let dlg = document.getElementById("dlgbox");
     let tblsize = document.getElementById("tablesize");
     let button = document.getElementById("buttonDsgn");
     let pertama = document.getElementsByTagName("p");
     let kedua = document.getElementsByTagName("p");
-    let buyButton = document.getElementsByTagName("button");
+    let DetailButton = document.getElementsByTagName("button");
     let availArray = [0, 2, 4, 6, 8, 10, 12, 14, 16];
     let sizeArray = [1, 3, 5, 7, 9, 11, 13, 15, 17];
     let availableStock;
@@ -111,68 +103,14 @@ require_once("config.php");
         tblsize.style.top = "90px";
     }
 
-    function changeInd() {
-        footerh3[0].innerHTML = "KONTAK KAMI";
-        footerh3[1].innerHTML = "Kirim Kami Pesan";
-        footerh3[3].innerHTML = "Telepon Kami";
-        footerh3[5].innerHTML = "Kami di Sosial Media";
-
-        for (let i = 0; i < 9; i++) {
-            availableStock = Math.floor(Math.random() * (100 - 1 + 1)) + 10;
-            pertama[availArray[i]].innerHTML = "Stok yang Tersedia : " + availableStock;
-            kedua[sizeArray[i]].innerHTML = "Ukuran : (2x2)m";
-            buyButton[i].innerHTML = "Beli";
-        }
-    }
-
-    function changeEng() {
-        footerh3[0].innerHTML = "CONTACT US";
-        footerh3[1].innerHTML = "Send Us Message";
-        footerh3[3].innerHTML = "Call Us";
-        footerh3[5].innerHTML = "We Are Social";
-
-        for (let i = 0; i < 9; i++) {
-            availableStock = Math.floor(Math.random() * (100 - 1 + 1)) + 10;
-            pertama[availArray[i]].innerHTML = "Available Stock : " + availableStock;
-            kedua[sizeArray[i]].innerHTML = "Size : (2x2)m";
-            buyButton[i].innerHTML = "Buy";
-        }
-    }
-
-    function showSize() {
-        whitebg.style.display = "block";
-        sizebox.style.display = "block";
-        dsgn.style.display = "none";
-
-        sizebox.style.left = "410px";
-        sizebox.style.top = "10px";
-    }
-
     function closeButton() {
         sizebox.style.display = "none";
         whitebg.style.display = "none";
         tablesize.style.display = "none";
     }
 
-    function showBuy() {
-        let discountValue = Math.floor(Math.random() * (15 - 10 + 1)) + 10;
-        let discountTotal = discountValue / 100 * 200;
-        document.getElementById("discount").innerHTML = "Rp" + discountTotal;
-        document.getElementById("totalPayment").innerHTML = "Rp" + (200 - discountTotal);
-
-        whitebg.style.display = "block";
-        buybox.style.display = "block";
-        dlg.style.display = "none";
-        dsgn.style.display = "none";
-        sizebox.style.display = "none";
-
-        buybox.style.top = "35px";
-        buybox.style.left = "450px";
-    }
-
-    function dlgOKBuy() {
+    function dlgOKDetail() {
         whitebg.style.display = "none";
-        buybox.style.display = "none";
         dlg.style.display = "none";
     }
 
