26991e7c6d0b90db80758e5448df48c0c2c4707c
Fluxhydra <32842814+Fluxhydra@users.noreply.github.com>
2018-05-29 21:38:47 +0700

    halaman about

---
+++ about.php
@@ -0,0 +1,109 @@
+<?php
+require_once ("config.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins|Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Basic" rel="stylesheet">
+    <title>Sample Shopping</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/Chart.js"></script>
+    <!--script src="js/modernizr.custom.js"></script-->
+
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet'
+          type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#theMenu">
+<?php include ("navbar.php"); ?>
+
+<!-- ========== HEADER SECTION ========== -->
+<section id="home"></section>
+<div id="headerwrap">
+    <div class="container bounceInDown animated">
+        <br>
+        <h1>Sample Shopping</h1>
+        <br>
+        <h4>Kami memberikan layanan belanja online yang terbaik bagi Anda</h4>
+        <h4>Kualitas barang sangat terjamin</h4>
+        <h4>Pelayanan MAXIMAL</h4>
+        <h4>Penjual yang terverifikasi</h4>
+        <h4>Kurir EXPRESS, pesan sekarang, besok sampai tujuan!</h4>
+        <br>
+        <h4>Tunggu apalagi?></h4>
+        <h4>Belanja sekarang juga di Sample Shopping!</h4>
+    </div><!-- /container -->
+</div><!-- /headerwrap -->
+
+<!-- ========== WHITE SECTION ========== -->
+<div id="w">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h4><strong>WE WORK HARD TO PROVIDE YOU WITH VARIOUS HIGH QUALITY ITEMS</strong></h4>
+            </div>
+        </div>
+    </div><!-- /container -->
+</div><!-- /w -->
+
+<!-- ========== FOOTER SECTION ========== -->
+<section id="contact"></section>
+<div id="f" style="background-color:#333">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3 class="text-center"><b>CONTACT US</b></h3>
+            </div>
+            <div class="col-sm-6">
+                <h3><b>Send Us A Message:</b></h3>
+                <h3 class="text-foot">turanwash@gmail.com</h3>
+                <br>
+            </div>
+
+            <div class="col-sm-6">
+                <h3><b>Call Us:</b></h3>
+                <h3 class="text-foot">+6281234567890</h3>
+                <br>
+            </div>
+        </div>
+    </div>  <!-- /container -->
+</div>      <!-- /f -->
+
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/custom.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
